03b0b83668e3e5ddfd9d77aa208a58b55d6b1736
Mateus Oliveira <mateusoli.viana@gmail.com>
2025-01-24 19:13:20 -0300

    feat: user model

---
+++ DotnetDapper/DotnetDapper/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace DotnetDapper.Models;
+
+public class User
+{
+    public long Id { get; set; }
+    public string FullName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public bool Actiove { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
