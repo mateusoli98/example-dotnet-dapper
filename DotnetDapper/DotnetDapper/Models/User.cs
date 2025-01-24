96e963e82b51457aa67dd1388eb76ce68e6e0e2d
Mateus Oliveira <mateusoli.viana@gmail.com>
2025-01-24 19:37:06 -0300

    fix: rename attribute 'Actiove' to 'Active'

--- DotnetDapper/DotnetDapper/Models/User.cs
+++ DotnetDapper/DotnetDapper/Models/User.cs
@@ -5,7 +5,7 @@ public class User
     public long Id { get; set; }
     public string FullName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
-    public bool Actiove { get; set; }
+    public bool Active { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
--- DotnetDapper/DotnetDapper/Models/UserResponseDTO.cs
+++ DotnetDapper/DotnetDapper/Models/UserResponseDTO.cs
@@ -5,6 +5,6 @@ public class UserResponseDTO
     public long Id { get; set; }
     public string FullName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
-    public bool Actiove { get; set; }
+    public bool Active { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
