5a67fc2fec4c98758615ed62e553555429c7fe57
Mateus Oliveira <mateusoli.viana@gmail.com>
2025-01-24 19:15:41 -0300

    feat: response model

---
+++ DotnetDapper/DotnetDapper/Models/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace DotnetDapper.Models;
+
+public class Response<T>
+{
+    public T? Result { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public bool Success { get; set; } = true;
+}
